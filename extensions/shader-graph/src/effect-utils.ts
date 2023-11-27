aa5d9bf449e73431490af481bb91a41779bd0189
knox <knox.h@qq.com>
2023-11-27 11:27:26 +0800

    shader graph  depend new  version change to 3.8.3

--- extensions/shader-graph/src/effect-utils.ts
+++ extensions/shader-graph/src/effect-utils.ts
@@ -1,7 +1,7 @@
 import { gte } from 'semver';
 
 export async function buildEffect(name: string, content: string) {
-    if (gte(Editor.App.version, '3.9.0')) {
+    if (gte(Editor.App.version, '3.8.3')) {
         return await Editor.Message.request('asset-db', 'execute-custom-operation', 'effect', 'build-effect', name, content);
     } else {
         return await Editor.Message.request('engine-extends', 'build-effect', name, content);
@@ -9,7 +9,7 @@ export async function buildEffect(name: string, content: string) {
 }
 
 export async function addChunk(name: string, content: string) {
-    if (gte(Editor.App.version, '3.9.0')) {
+    if (gte(Editor.App.version, '3.8.3')) {
         await Editor.Message.request('asset-db', 'execute-custom-operation', 'effect', 'add-chunk', name, content);
     } else {
         return await Editor.Message.request('engine-extends', 'add-chunk', name, content);
